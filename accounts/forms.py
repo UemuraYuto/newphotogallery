77235d979eca7a484e42f9c66914aba175543d27
65-74231\USER <Yuto_Uemura@totec.co.jp>
2023-06-01 17:28:48 +0900

    383まで完成

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,14 +6,15 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     '''UserCreationFormのサブクラス
     '''
-    '''UserCreationFormのインナークラス
+    class Meta:
+        '''UserCreationFormのインナークラス
         
-    Attributes:
-    model:連携するUserモデル
-    fields:フォームで使用するフィールド
-    '''
-    # 連携するUserモデルを設定
-    model = CustomUser
-    # フォームで使用するフィールドを設定
-    # ユーザー名、メールアドレス、パスワード、パスワード(確認用)
-    fields = ('username', 'email', 'password1', 'password2')
+        Attributes:
+          model:連携するUserモデル
+          fields:フォームで使用するフィールド
+        '''
+        # 連携するUserモデルを設定
+        model = CustomUser
+        # フォームで使用するフィールドを設定
+        # ユーザー名、メールアドレス、パスワード、パスワード(確認用)
+        fields = ('username', 'email', 'password1', 'password2')
