c44b04beae6b6c0fd57d4b15f5c7af7f83a95545
65-74231\USER <Yuto_Uemura@totec.co.jp>
2023-06-01 13:54:13 +0900

    P,364 settings.py

--- photoproject/settings.py
+++ photoproject/settings.py
@@ -129,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Userモデルの代わりにCustomUserモデルを使用する
+AUTH_USER_MODEL = 'accounts.CustomUser'
