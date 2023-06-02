98e96930600e81bc19957d964f2d90991b9158be
65-74231\USER <Yuto_Uemura@totec.co.jp>
2023-06-02 14:32:17 +0900

    451まで完了

--- photo/views.py
+++ photo/views.py
@@ -13,7 +13,7 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
 
-class IndexView(ListView):
+class IndexView(TemplateView):
     '''トップページのビュー
     '''
     # index.htmlをレンダリングする
